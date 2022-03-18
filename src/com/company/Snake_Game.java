047fb27b55e53650be7e960de52e33946bd91c69
ma211169 <ma211169@reigate.ac.uk>
2022-03-18 10:10:15 +0000

    Initial commit

---
+++ src/com/company/Snake_Game.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.util.Scanner;
+
+
+    public class Snake_Game {
+        public static void main(String[] args) {
+
+            new GameFrame(); //call GameFrame class
+        }
+    }
